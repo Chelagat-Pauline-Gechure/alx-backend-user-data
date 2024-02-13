7e4f8915b274eb20a4ab2d468059eaca784b7549
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-02-13 13:01:55 +0300

    Fixed error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-"""Authentication module.
+""" Authentication Module
 """
-from flask import request
 from typing import List, TypeVar
+from flask import request
 import fnmatch
 
 
 class Auth:
-    """Authentication class.
+    """ Class to manage API authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required.
+        """ Check if authentication is required.
+        Args:
+            paths(str): Path being accessed.
+            excluded_paths (List[str]): List of paths not to be autheniticated.
+        Returns:
+            bool: True if authentication is required, false otherwise.
         """
         if path is None:
             return True
@@ -25,13 +30,23 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get authorization header.
+        """
+        Method to retrieve authorization header from Flask request object.
+        Args:
+            request(flask.request): Flask request object.
+        Returns:
+            str: Authorization header, or None if not found.
         """
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get user from request.
+        """
+        Method to retrieve current user based on Flask request object.
+        Args:
+            request(flask.request): Flask request object.
+        Returns:
+            TypeVar('User'): Current user or None if not found.
         """
         return None
