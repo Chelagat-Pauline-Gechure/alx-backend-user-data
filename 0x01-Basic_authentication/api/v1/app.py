bdf8e2a1c0ff16b0131cc43c2c8fac45080670ed
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-02-13 07:33:30 +0000

    Fixed app.py error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,7 +25,7 @@ def not_found(error) -> str:
 def unauthorized(error) -> str:
     """ Unauthorized error handler
     """
-    return jsonify({"error":"Unauthorized"}), 401
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
