c7c5fcad9f8826c409a35258c1a0098749e4a9e7
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-02-13 09:17:52 +0000

    3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,16 +1,73 @@
 #!/usr/bin/env python3
-""" Modules required
+""" Authentication Module
 """
+
+from typing import List, TypeVar
 from flask import request
 
+
 class Auth:
+    """ Class to manage API authentication
+    """
+
+
     def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
-        """ Check if authentication is required."""
-        return False
+        """ Check if authentication is required.
+        
+        Args:
+            paths(str): Path being accessed.
+            excluded_paths (List[str]): List of paths not to be autheniticated.
+
+        Returns:
+            bool: True if authentication is required, false otherwise.
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """ Return authorization header."""
+        """
+        Method to retrieve authorization header from Flask request object.
+        
+        Args:
+            request(flask.request): Flask request object.
+
+        Returns:
+            str: Authorization header, or None if not found.
+        """
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+
+        if header is None:
+            return None
+
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Retrun the current user."""
+        """
+        Method to retrieve current user based on Flask request object.
+        
+        Args:
+            request(flask.request): Flask request object.
+
+        Returns:
+            TypeVar('User'): Current user or None if not found.
+        """
         return None
