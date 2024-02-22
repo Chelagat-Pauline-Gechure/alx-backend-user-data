786dc2bfdd52428f9063c90bf5ccaa731885cbed
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-02-22 13:06:45 +0000

    Added new line in user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -21,3 +21,4 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
